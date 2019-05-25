48d8a208c0bf31018d920baaebab476a837152cb
yasas1 <32437531+yasas1@users.noreply.github.com>
2019-05-25 13:52:28 +0530

    canny edge detection

--- CannyEdgeDetection/CannyEdgeDetection/CannyEdgeDetection.cpp
+++ CannyEdgeDetection/CannyEdgeDetection/CannyEdgeDetection.cpp
@@ -12,30 +12,41 @@
 using namespace std;
 using namespace cv;
 
-void cannyEdgeDetec(Mat img)
+void cannyEdgeDetec(Mat img, int threshold1, String outputName)
 {
 	Mat src = img;
+	int threshold = threshold1;
 	//src = imread("C:/Users/Dell/Desktop/data_dir/Fish/img/0001.jpg");
-	namedWindow("Original image");
-	imshow("Original image", src);
+	namedWindow("Original image" + outputName);
+	imshow("Original image" + outputName, src);
 
 	Mat gray, edge, draw;
 	cvtColor(src, gray, COLOR_BGR2GRAY);
 
-	Canny(gray, edge, 50, 150, 3);
+	Canny(gray, edge, threshold, threshold*3, 3);
 
 	edge.convertTo(draw, CV_8U);
-	namedWindow("image", WINDOW_AUTOSIZE);
-	imshow("image", draw);
+	namedWindow("image" + outputName, WINDOW_AUTOSIZE);
+	imshow("image" + outputName, draw);
 
-	waitKey(0);
+	imwrite("C:/Users/Dell/Desktop/output2/"+ outputName +".jpg", draw);
+
 }
 
 int main()
 {
-	Mat img_1, img_2;
+	Mat img_1, img_2; 
+	
 	img_1 = imread("C:/Users/Dell/Desktop/data_dir/Fish/img/0001.jpg");
 
-	cannyEdgeDetec(img_1);
+	cannyEdgeDetec(img_1,50,"output3");
+
+	waitKey(0);
+
+	img_2 = imread("C:/Users/Dell/Desktop/data_dir/Fish/img/0199.jpg");
+
+	cannyEdgeDetec(img_2, 50, "output4");
+
+	waitKey(0);
 }
 
